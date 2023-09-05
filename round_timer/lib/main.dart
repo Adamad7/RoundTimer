2254ef39ccb49df11906457492bf3a4164eaa143
Adam Mazur <adam.mazur951@gmail.com>
2023-09-05 19:26:30 +0200

    Created custom clipping path for dial

--- round_timer/lib/main.dart
+++ round_timer/lib/main.dart
@@ -49,28 +49,27 @@ class _MainScreenState extends State<MainScreen> {
       ),
       body: SizedBox.expand(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              width: 200,
-              height: 400,
+              width: 300,
+              height: 500,
               child: Stack(alignment: Alignment.centerRight, children: [
                 ChangeNotifierProvider(
                   create: (context) => TimeDialModel(items: getNumbers(), numberOfVisibleItems: 11),
-                  child: const TimeDial(height: 400, width: 200),
+                  child: const TimeDial(height: 450),
                 ),
                 ChangeNotifierProvider(
                   create: (context) => TimeDialModel(items: getNumbers(), numberOfVisibleItems: 11),
-                  child: const TimeDial(height: 340, width: 170),
+                  child: const TimeDial(height: 380),
                 ),
                 ChangeNotifierProvider(
                   create: (context) => TimeDialModel(items: getNumbers(), numberOfVisibleItems: 11),
-                  child: const TimeDial(height: 280, width: 140),
+                  child: const TimeDial(height: 310),
                 ),
               ]),
-            )
-            // TimeDial(height: 400, width: 200),
+            ),
           ],
         ),
       ),
@@ -80,7 +79,7 @@ class _MainScreenState extends State<MainScreen> {
 }
 
 class TimeDial extends StatefulWidget {
-  const TimeDial({super.key, required this.width, required this.height});
+  const TimeDial({super.key, required this.height}) : width = height / 2;
   final double width;
   final double height;
 
@@ -89,36 +88,7 @@ class TimeDial extends StatefulWidget {
 }
 
 class _TimeDialState extends State<TimeDial> with SingleTickerProviderStateMixin {
-  // List<String> getNumbers() {
-  //   List<String> numbers = [];
-  //   for (int i = 0; i < 10; i++) {
-  //     numbers.add(" ${i.toString()}");
-  //   }
-  //   for (int i = 10; i < 60; i++) {
-  //     numbers.add(i.toString());
-  //   }
-  //   return numbers;
-  // }
-
   late TimeDialModel dialModel;
-  // final int numberOfVisibleNumbers = 7;
-  // late final anglePerNumber = pi / numberOfVisibleNumbers;
-  // int selectedItem = 0;
-  // late final int additionalItemsBuffer = 2;
-  // late List<String> visibleItems =
-  //     List.filled(numberOfVisibleNumbers + 2 * additionalItemsBuffer, '0');
-
-  // void getVisibleNumbers() {
-  //   List<String> allNumbers = getNumbers();
-
-  //   var startIndex = selectedItem - numberOfVisibleNumbers ~/ 2;
-  //   startIndex -= additionalItemsBuffer;
-  //   for (int i = 0; i < numberOfVisibleNumbers + 2 * additionalItemsBuffer; i++) {
-  //     visibleItems[i] = allNumbers[(startIndex + i) % allNumbers.length];
-  //   }
-  // }
-
-  // double angle = 0;
   late AnimationController controller;
   late Animation<double> animation;
 
@@ -127,31 +97,18 @@ class _TimeDialState extends State<TimeDial> with SingleTickerProviderStateMixin
     super.initState();
     controller = AnimationController(duration: const Duration(milliseconds: 100), vsync: this);
     dialModel = Provider.of<TimeDialModel>(context, listen: false);
-    // getVisibleNumbers();
   }
 
   void animateToClosestNumber() {
-    // double closestAngle = 0;
-    // if (angle > anglePerNumber / 2) {
-    //   closestAngle = anglePerNumber;
-    //   selectedItem = (selectedItem + 1) % 60;
-    // } else if (angle < -anglePerNumber / 2) {
-    //   closestAngle = -anglePerNumber;
-    //   selectedItem = (selectedItem - 1) % 60;
-    // }
-
     animation =
         Tween<double>(begin: dialModel.angle, end: dialModel.closestAngle).animate(controller)
           ..addListener(() {
             dialModel.angle = animation.value;
-            // setState(() {
-            // });
           })
           ..addStatusListener((status) {
             if (status == AnimationStatus.completed) {
               dialModel.getVisibleNumbers();
               dialModel.angle = 0;
-              // setState(() {});
             }
           });
     controller.reset();
@@ -167,47 +124,38 @@ class _TimeDialState extends State<TimeDial> with SingleTickerProviderStateMixin
         decoration: const BoxDecoration(
           borderRadius:
               BorderRadius.only(topLeft: Radius.circular(360), bottomLeft: Radius.circular(360)),
+          color: Colors.blue,
         ),
-        child: GestureDetector(
-          onPanUpdate: (details) {
-            dialModel.addAngle((details.delta.dy -
-                    (details.localPosition.dy > widget.height / 2 ? -1 : 1) * details.delta.dx) /
-                widget.width);
-            // angle += (details.delta.dy -
-            //         (details.localPosition.dy > widget.height / 2 ? -1 : 1) * details.delta.dx) /
-            //     widget.width;
-            // if (angle >= anglePerNumber) {
-            //   angle = 0;
-            //   selectedItem = (selectedItem + 1) % 60;
-            //   getVisibleNumbers();
-            // } else if (angle <= -anglePerNumber) {
-            //   angle = 0;
-            //   selectedItem = (selectedItem - 1) % 60;
-            //   getVisibleNumbers();
-            // }
-
-            // setState(() {});
-          },
-          onPanEnd: (details) => animateToClosestNumber(),
-          child: Stack(
-            children: [
-              CustomPaint(
-                size: Size(widget.width, widget.height),
-                painter: DialBackgroundPainter(color: Colors.red, width: 15),
-              ),
-              Consumer<TimeDialModel>(
-                builder: (context, model, child) {
-                  return CustomPaint(
-                    size: Size(widget.width, widget.height),
-                    painter: DialNumbersPainter(
-                        numbers: model.visibleItems,
-                        angle: model.angle,
-                        leftHanded: false,
-                        anglePerNumber: model.anglePerItem),
-                  );
-                },
-              ),
-            ],
+        child: ClipPath(
+          clipper: DialClipper(dialWidth: 20),
+          child: GestureDetector(
+            onPanUpdate: (details) {
+              dialModel.addAngle((details.delta.dy -
+                      (details.localPosition.dy > widget.height / 2 ? -1 : 1) * details.delta.dx) /
+                  widget.width);
+            },
+            onPanEnd: (details) => animateToClosestNumber(),
+            child: Stack(
+              children: [
+                CustomPaint(
+                  size: Size(widget.width, widget.height),
+                  painter: DialBackgroundPainter(color: Colors.red, width: 20),
+                ),
+                Consumer<TimeDialModel>(
+                  builder: (context, model, child) {
+                    return CustomPaint(
+                      size: Size(widget.width, widget.height),
+                      painter: DialNumbersPainter(
+                          numbers: model.visibleItems,
+                          angle: model.angle,
+                          leftHanded: false,
+                          anglePerNumber: model.anglePerItem,
+                          dialWidth: 20),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ));
   }
@@ -218,11 +166,13 @@ class DialNumbersPainter extends CustomPainter {
       {required this.numbers,
       required this.angle,
       this.leftHanded = false,
-      required this.anglePerNumber});
+      required this.anglePerNumber,
+      required this.dialWidth});
   final double angle;
   final List<String> numbers;
   final bool leftHanded;
   final double anglePerNumber;
+  final double dialWidth;
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return angle != (oldDelegate as DialNumbersPainter).angle;
@@ -230,7 +180,7 @@ class DialNumbersPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    const textStyle = TextStyle(color: Colors.white, fontSize: 13);
+    const textStyle = TextStyle(color: Colors.white, fontSize: 15);
     final textPainter = TextPainter(textDirection: TextDirection.ltr);
     canvas.translate(size.width, size.height / 2);
     canvas.rotate(-pi / 2 - angle + anglePerNumber / 2 - anglePerNumber * 2);
@@ -239,8 +189,8 @@ class DialNumbersPainter extends CustomPainter {
       textPainter.text = TextSpan(text: numbers[i], style: textStyle);
       textPainter.layout();
 
-      final textOffset = Offset(
-          textPainter.width / 2 - textPainter.width / 2, size.height / 2 - textPainter.height / 2);
+      final textOffset =
+          Offset((dialWidth - textPainter.width) / 2, size.height / 2 - textPainter.height / 2);
       textPainter.paint(canvas, textOffset);
       canvas.translate(size.width, size.height / 2);
       canvas.rotate(anglePerNumber);
@@ -274,6 +224,43 @@ class DialBackgroundPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class DialClipper extends CustomClipper<Path> {
+  DialClipper({required this.dialWidth});
+  final double dialWidth;
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width, size.height - dialWidth);
+    path.lineTo(size.width, size.height);
+    path.arcTo(
+        Rect.fromCenter(
+            center: Offset(size.width, size.height / 2),
+            width: size.width * 2,
+            height: size.height),
+        0.5 * pi,
+        pi,
+        true);
+    path.moveTo(size.width, 0);
+    path.lineTo(size.width, dialWidth);
+
+    path.arcTo(
+        Rect.fromCenter(
+            center: Offset(size.width, size.height / 2),
+            width: size.width * 2 - dialWidth * 2,
+            height: size.height - dialWidth * 2),
+        1.5 * pi,
+        -pi,
+        true);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
     return false;
   }
 }
